e25168934d324b1c9f2b1c517569cbf266af97b3
Zangura <zanguraa@gmail.com>
2024-01-02 19:10:51 +0400

    added SelectedWork component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,9 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Profile from "../assets/images/profile.jpg";
 import FadeIn from "./UseFadeIn";
+import { TypeAnimation } from "react-type-animation";
 
 // Define the interface for props
 interface Props {
@@ -11,7 +13,7 @@ interface Props {
 // Use React.FunctionComponent or React.FC to define the function component
 const Hero: React.FunctionComponent<Props> = ({ name }) => {
   return (
-    <div className="flex flex-col gap-8 justify-center items-center py-[120px] px-8 lg:px-20 lg:flex-row">
+    <div className="flex flex-col gap-8 justify-center items-center py-[50px] px-8 lg:px-20 lg:flex-row">
       <FadeIn>
         <h1 className="font-['Alerion-Black'] uppercase text-center lg:text-8xl ">
           Hello, I&apos;m {name}
@@ -29,7 +31,19 @@ const Hero: React.FunctionComponent<Props> = ({ name }) => {
         </div>
         <FadeIn>
           <h2 className="font-['Alerion'] uppercase text-base	text-center	lg:text-xl	">
-            A graphic designer focus on UX Design & Branding.
+            <TypeAnimation
+              sequence={[
+                "A graphic designer", // Types 'One'
+                1000, // Waits 1s
+                "A graphic designer focus on", // Deletes 'One' and types 'Two'
+                2000, // Waits 2s
+                "A graphic designer focus on UX Design",
+                2000, // Types 'Three' without deleting 'Two'
+                "A graphic designer focus on UX Design & Branding.",
+              ]}
+              wrapper="span"
+              cursor={true}
+            />
           </h2>
         </FadeIn>
       </div>
---
+++ src/components/SelectedWork.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SelectedWork = () => {
+  return (
+    <div>
+        <div></div>
+    </div>
+  )
+}
+
+export default SelectedWork
